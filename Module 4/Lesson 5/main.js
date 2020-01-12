6a749e427624f3520ba17a839f81c2714048872e
Valentin Ageev <sazuru@gmail.com>
2020-01-12 17:51:41 +0300

    Completed Lesson 4 task 5

---
+++ Module 4/Lesson 5/main.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+  function emptyTask() {
+    if ($("div").is(".task-item") == true) {
+      $(".empty-item").css("display", "none");
+    } else {
+      $(".empty-item").css("display", "block");
+    }
+  }
+
+  $(".task-confirm").click(function() {
+    if ($("input").val() !== "" && $("textarea").val() !== "") {
+      $("input").removeClass("error");
+      $("textarea").removeClass("error");
+      var newTask = $(`
+        <div class="task-item">
+          <div class="task-name">
+            <div class="task-item-title">
+              <div class="close-task-item">
+              </div>
+            </div>
+                <div class="hide-task-item">
+                </div>
+          </div>
+          <div class="task-description">
+          </div>
+        </div>`);
+      var title = $("<h3></h3>");
+      var description = $("<p></p>");
+
+      title.text($("input").val());
+      description.text($("textarea").val());
+
+      $(".task-list").append(newTask);
+      $(".task-item-title:last").prepend(title);
+      $(".task-description:last").prepend(description);
+
+      $("input").val("");
+      $("textarea").val("");
+    } else {
+      $("input").addClass("error");
+      $("textarea").addClass("error");
+    }
+
+    emptyTask();
+  });
+
+  $("body").on("click", ".close-task-item", function() {
+    setTimeout(emptyTask, 300);
+    $(this)
+      .parents(".task-item")
+      .remove();
+  });
+  $("body").on("click", ".hide-task-item", function() {
+    $(this).toggleClass("rotate");
+    $(this)
+      .parents(".task-item")
+      .children(".downblock")
+      .toggle("hide");
+  });
+});
