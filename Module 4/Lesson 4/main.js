ad70539246d420b0947614313f877f2ee8175f69
Valentin Ageev <sazuru@gmail.com>
2020-01-12 17:46:57 +0300

    Refactored lesson 4 task 4

--- Module 4/Lesson 4/main.js
+++ Module 4/Lesson 4/main.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function() {
   function emptyTask() {
     if ($("div").is(".task-item") == true) {
       $(".empty-item").css("display", "none");
@@ -7,38 +7,45 @@ $(function() {
     }
   }
 
-  $(".confirm").click(function() {
-    $(".task-item-title h3").each(function() {
-      if ($(this).text() == $("input").val()) {
-        $("input").val("");
-        $("textarea").val("");
-        return false;
-      }
-    });
-
-    if ($("input").val() != "") {
-      var newItem = $(
-        '<div class="task-item"><div class="upblock"><div class="task-item-title"><div class="close-task-item"></div></div><div class="hide-task-item"></div></div><div class="downblock"></div></div>'
-      );
+  $(".task-confirm").click(function() {
+    if ($("input").val() !== "" && $("textarea").val() !== "") {
+      $("input").removeClass("error");
+      $("textarea").removeClass("error");
+      var newTask = $(`
+        <div class="task-item">
+          <div class="task-name">
+            <div class="task-item-title">
+              <div class="close-task-item">
+              </div>
+            </div>
+                <div class="hide-task-item">
+                </div>
+          </div>
+          <div class="task-description">
+          </div>
+        </div>`);
       var title = $("<h3></h3>");
-      var note = $("<p></p>");
+      var description = $("<p></p>");
 
       title.text($("input").val());
-      note.text($("textarea").val());
+      description.text($("textarea").val());
 
-      $(".task-list").append(newItem);
+      $(".task-list").append(newTask);
       $(".task-item-title:last").prepend(title);
-      $(".downblock:last").prepend(note);
+      $(".task-description:last").prepend(description);
 
       $("input").val("");
       $("textarea").val("");
+    } else {
+      $("input").addClass("error");
+      $("textarea").addClass("error");
     }
 
     emptyTask();
   });
 
   $("body").on("click", ".close-task-item", function() {
-    setTimeout(emptyTask, 600);
+    setTimeout(emptyTask, 300);
     $(this)
       .parents(".task-item")
       .remove();
