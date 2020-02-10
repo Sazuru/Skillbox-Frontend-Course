6bfacbe7b28b8ce54dfc63b026bb9a7d3df9afa5
Valentin Ageev <sazuru@gmail.com>
2020-02-10 23:07:07 +0300

    All works done with portfolio

--- Module 7/Portfolio/js/main.js
+++ Module 7/Portfolio/js/main.js
@@ -1,3 +1,12 @@
+"use strict";
+
+var templateMailRecall = "Ваше имя: {{name}}<br />Телефон: {{phone}}";
+var uriMailRecall = "mailto:sazuru@gmail.comu?subject=Заказ%20звонка&body=";
+var templateMailChat =
+  "Ваше имя: {{name}}<br />Телефон: {{phone}}<br />Почта: {{mail}}";
+var uriMailChat = "mailto:sazuru@gmail.com?subject=Узнать%20больше&body=";
+var template;
+
 $(document).ready(function() {
   // slick слайдер
   $(".main-works__examples").slick({
@@ -34,8 +43,88 @@ $(document).ready(function() {
   $(function() {
     $("a[href^='#']").click(function() {
       var _href = $(this).attr("href");
-      $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+      $("html, body").animate({
+        scrollTop: $(_href).offset().top + "px"
+      });
       return false;
     });
+
+    var popupChat = $("#popup-chat");
+    var popupRecall = $("#popup-recall");
+    var popupBG = $("#popup_bg");
+
+    $(".phone-number").mask("+7 (999) 999-99-99");
+
+    // включение мобильного меню
+    var menuList = $(".top-menu__list_menu");
+    $(menuList).click(function() {
+      if (menuList.css("height") != "0px") {
+        menuList.removeClass("show-mobile-menu");
+      }
+    });
+
+    $("#burger-menu").click(function() {
+      menuList.addClass("show-mobile-menu");
+    });
+
+    $(window).resize(function() {
+      menuList.removeClass("show-mobile-menu");
+    });
+
+    //вызов модальных окон
+    $(".callback-button").click(function() {
+      popupRecall.fadeIn();
+      popupBG.fadeIn();
+      document.body.style.overflow = "hidden";
+    });
+
+    $(".button-buy").click(function() {
+      popupChat.fadeIn();
+      popupBG.fadeIn();
+      document.body.style.overflow = "hidden";
+    });
+
+    // валидация форм и отправка писем
+    $("#button_send-recall").click(function(e) {
+      var userName = $("#form-recall_name-input");
+      var phoneNumber = $("#form-recall_phone-input");
+
+      validate(userName, phoneNumber);
+
+      if (userName.val() && phoneNumber.val()) {
+        (template = templateMailRecall.replace("{{name}}", userName.val())),
+          (template = template.replace("{{phone}}", phoneNumber.val()));
+
+        window.open(uriMailRecall + template, "_blank");
+      }
+      e.preventDefault();
+    });
+
+    $("#button_send-chat").click(function(e) {
+      var userName = $("#form-chat_name-input");
+      var phoneNumber = $("#form-chat_phone-input");
+      var email = $("#form-chat_email-input");
+
+      validate(userName, phoneNumber, email);
+
+      if (userName.val() && phoneNumber.val() && email.val()) {
+        (template = templateMailChat.replace("{{name}}", userName.val())),
+          (template = template.replace("{{phone}}", phoneNumber.val())),
+          (template = template.replace("{{mail}}", email.val()));
+
+        window.open(uriMailChat + template, "_blank");
+      }
+      e.preventDefault();
+    });
   });
+
+  function validate() {
+    [].forEach.call(arguments, function(element) {
+      if (element.attr("required") && !element.val()) {
+        element.css("outline", "2px solid red");
+      } else {
+        element.css("outline", "none");
+      }
+    });
+  }
 });
