f7bcc4b4e0d27b66f909cd6949e605ef040d1421
Valentin Ageev <sazuru@gmail.com>
2020-02-21 22:01:54 +0300

    Changes at popup forms

--- Module 7/Portfolio/js/main.js
+++ Module 7/Portfolio/js/main.js
@@ -1,12 +1,4 @@
 "use strict";
-
-var templateMailRecall = "Ваше имя: {{name}}<br />Телефон: {{phone}}";
-var uriMailRecall = "mailto:sazuru@gmail.comu?subject=Заказ%20звонка&body=";
-var templateMailChat =
-  "Ваше имя: {{name}}<br />Телефон: {{phone}}<br />Почта: {{mail}}";
-var uriMailChat = "mailto:sazuru@gmail.com?subject=Узнать%20больше&body=";
-var template;
-
 $(document).ready(function() {
   // slick слайдер
   $(".main-works__examples").slick({
@@ -24,8 +16,8 @@ $(document).ready(function() {
         breakpoint: 1070,
         settings: {
           slidesToShow: 2,
-          dots: true
-        }
+          dots: true,
+        },
       },
       {
         breakpoint: 600,
@@ -33,10 +25,10 @@ $(document).ready(function() {
           slidesToShow: 1,
           slidesToScroll: 1,
           dots: true,
-          arrows: false
-        }
-      }
-    ]
+          arrows: false,
+        },
+      },
+    ],
   });
 
   // Плавный скроллинг к разделам
@@ -44,7 +36,7 @@ $(document).ready(function() {
     $("a[href^='#']").click(function() {
       var _href = $(this).attr("href");
       $("html, body").animate({
-        scrollTop: $(_href).offset().top + "px"
+        scrollTop: $(_href).offset().top + "px",
       });
       return false;
     });
@@ -52,26 +44,9 @@ $(document).ready(function() {
     $(".main-about__learn-more").on("click", function() {
       var target = $("#skills");
       var pos = $(target).offset().top;
-      $("html, body").animate({ scrollTop: pos - 100 });
-    });
-
-    var popupChat = $("#popup-chat");
-    var popupRecall = $("#popup-recall");
-    var popupBG = $("#popup_bg");
-
-    $(".phone-number").mask("+7 (999) 999-99-99");
-
-    //вызов модальных окон
-    $(".callback-button").click(function() {
-      popupRecall.fadeIn();
-      popupBG.fadeIn();
-      document.body.style.overflow = "hidden";
-    });
-
-    $(".button-buy").click(function() {
-      popupChat.fadeIn();
-      popupBG.fadeIn();
-      document.body.style.overflow = "hidden";
+      $("html, body").animate({
+        scrollTop: pos - 100,
+      });
     });
 
     //Появление мобильного меню
@@ -83,48 +58,29 @@ $(document).ready(function() {
     $(window).resize(function() {
       $(".mobile__menu").slideUp(0);
     });
-
-    // валидация форм и отправка писем
-    $("#button_send-recall").click(function(e) {
-      var userName = $("#form-recall_name-input");
-      var phoneNumber = $("#form-recall_phone-input");
-
-      validate(userName, phoneNumber);
-
-      if (userName.val() && phoneNumber.val()) {
-        (template = templateMailRecall.replace("{{name}}", userName.val())),
-          (template = template.replace("{{phone}}", phoneNumber.val()));
-
-        window.open(uriMailRecall + template, "_blank");
-      }
-      e.preventDefault();
-    });
-
-    $("#button_send-chat").click(function(e) {
-      var userName = $("#form-chat_name-input");
-      var phoneNumber = $("#form-chat_phone-input");
-      var email = $("#form-chat_email-input");
-
-      validate(userName, phoneNumber, email);
-
-      if (userName.val() && phoneNumber.val() && email.val()) {
-        (template = templateMailChat.replace("{{name}}", userName.val())),
-          (template = template.replace("{{phone}}", phoneNumber.val())),
-          (template = template.replace("{{mail}}", email.val()));
-
-        window.open(uriMailChat + template, "_blank");
-      }
-      e.preventDefault();
-    });
+  });
+  
+  // popups
+  $(".callback-button, .mobile-callback-button").click(function(e) {
+    e.preventDefault();
+    $(".popup-callback").css("display", "flex");
+    $("body").toggleClass("hidden");
   });
 
-  function validate() {
-    [].forEach.call(arguments, function(element) {
-      if (element.attr("required") && !element.val()) {
-        element.css("outline", "2px solid red");
-      } else {
-        element.css("outline", "none");
-      }
-    });
-  }
+  $(".button-buy").click(function(e) {
+    e.preventDefault();
+    $(".popup-find-out-more").css("display", "flex");
+    $("body").toggleClass("hidden");
+  });
+
+  $(".popup-callback, .popup-find-out-more").click(function(e) {
+    if (event.target == this) {
+      $(this).hide();
+      $("body").toggleClass("hidden");
+    }
+  });
+
+  $(".popup-callback__submit, .popup-find-out-more__submit").click(function(e) {
+    e.preventDefault();
+  });
 });
