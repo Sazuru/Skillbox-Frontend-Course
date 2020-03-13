268ac786ec3da5bd3fc223137d26a58e7cff7aec
Valentin Ageev <sazuru@gmail.com>
2020-03-13 18:26:49 +0300

    fixing bugs, add features

--- Module 7/Portfolio/js/main.js
+++ Module 7/Portfolio/js/main.js
@@ -1,13 +1,15 @@
-"use strict";
+'use strict';
 $(document).ready(function() {
   // slick слайдер
-  $(".main-works__examples").slick({
+  $('.main-works__examples').slick({
     infinite: true,
+    draggable: true,
     slidesToShow: 3,
-    speed: 1000,
+    slidesToScroll: 4,
+    speed: 300,
     slidesToScroll: 1,
     autoplay: false,
-    cssEase: "ease-in-out",
+    cssEase: 'ease-in-out',
     dots: false,
     nextArrow: '<button class="next"></button>',
     prevArrow: '<button class="prev"></button>',
@@ -34,68 +36,60 @@ $(document).ready(function() {
   // Плавный скроллинг к разделам
   $(function() {
     $("a[href^='#']").click(function() {
-      var _href = $(this).attr("href");
-      $("html, body").animate({
-        scrollTop: $(_href).offset().top + "px",
+      var _href = $(this).attr('href');
+      $('html, body').animate({
+        scrollTop: $(_href).offset().top + 'px',
       });
       return false;
     });
 
-    $(".main-about__learn-more").on("click", function() {
-      var target = $("#skills");
-      var pos = $(target).offset().top;
-      $("html, body").animate({
-        scrollTop: pos - 100,
-      });
-    });
-
     //Появление мобильного меню
-    $(".mobile-menu-button").click(function(e) {
+    $('.mobile-menu-button').click(function(e) {
       e.preventDefault();
-      $(".mobile__menu").slideToggle();
+      $('.mobile__menu').slideToggle();
     });
 
     $(window).resize(function() {
-      $(".mobile__menu").slideUp(0);
+      $('.mobile__menu').slideUp(0);
     });
   });
 
   // popups
-  $(".callback-button, .mobile-callback-button").click(function(e) {
+  $('.callback-button, .mobile-callback-button').click(function(e) {
     e.preventDefault();
-    $(".popup-callback").css("display", "flex");
-    $("body").toggleClass("hidden");
+    $('.popup-callback').css('display', 'flex');
+    $('body').toggleClass('hidden');
   });
 
-  $(".button-buy").click(function(e) {
+  $('.button-buy').click(function(e) {
     e.preventDefault();
-    $(".popup-find-out-more").css("display", "flex");
-    $("body").toggleClass("hidden");
+    $('.popup-find-out-more').css('display', 'flex');
+    $('body').toggleClass('hidden');
   });
 
-  $(".popup-callback, .popup-find-out-more").click(function(e) {
+  $('.popup-callback, .popup-find-out-more').click(function(e) {
     if (event.target == this) {
       $(this).hide();
-      $("body").toggleClass("hidden");
+      $('body').toggleClass('hidden');
     }
   });
 
   //Отправка данных
-  $(".popup-callback__form, .popup-find-out-more__form").submit(function() {
+  $('.popup-callback__form, .popup-find-out-more__form').submit(function() {
     //Change
     var th = $(this);
     $.ajax({
-      type: "POST",
-      url: "mail.php", //Change
+      type: 'POST',
+      url: 'mail.php', //Change
       data: th.serialize(),
     }).done(function() {
-      alert("Спасибо!");
+      alert('Спасибо!');
       setTimeout(function() {
         // Done Functions
-        th.trigger("reset");
-        $(".popup-callback").css("display", "none");
-        $(".popup-find-out-more").css("display", "none");
-        $("body").toggleClass("hidden");
+        th.trigger('reset');
+        $('.popup-callback').css('display', 'none');
+        $('.popup-find-out-more').css('display', 'none');
+        $('body').toggleClass('hidden');
       }, 1000);
     });
     return false;
